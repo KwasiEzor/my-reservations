8a0acc215b02743a64b0907944e1c58d4d018a1d
kwasiezor <kwasiezor@gmail.com>
2021-07-26 19:58:41 +0200

    first commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 5500),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ee0a86df5ca89263136d065bd0142fd9'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a4241210-8919-43af-8678-e42e277b11fd'
+};
